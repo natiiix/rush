17ab4dbdcd3925db4fbba8b9a9c162db67c211a8
Ivo Meixner <ivo.meixner@se7en.cz>
2018-04-14 02:32:54 +0200

    Add comments

--- src/main.cpp
+++ src/main.cpp
@@ -8,29 +8,51 @@
 
 int main(int argc, char *argv[])
 {
+    // Open the temporary file
     std::ofstream tmpFile(TMP_PATH);
 
-    if (tmpFile.is_open())
+    // Unable to open the temporary file
+    if (!tmpFile.is_open())
     {
-        for (int i = 1; i < argc; i++)
-        {
-            std::cout << "Reading source file: " << argv[i] << std::endl;
-            std::ifstream sourceFile(argv[i]);
-
-            if (sourceFile.is_open())
-            {
-                std::string line;
-                while (std::getline(sourceFile, line))
-                {
-                    tmpFile << line << std::endl;
-                }
-            }
-        }
-
-        tmpFile.close();
-        system("c++ -o rush_output.exe " TMP_PATH);
-        remove(TMP_PATH);
+        return -1;
     }
 
+    // Iterate through the source files
+    for (int i = 1; i < argc; i++)
+    {
+        std::cout << "Reading source file: " << argv[i] << std::endl;
+        std::ifstream sourceFile(argv[i]);
+
+        if (!sourceFile.is_open())
+        {
+            std::cerr << "Unable to read source file \"" << argv[1] << "\"!" << std::endl;
+            continue;
+        }
+
+        std::string line;
+        while (std::getline(sourceFile, line))
+        {
+            tmpFile << line << std::endl;
+        }
+
+        sourceFile.close();
+    }
+
+    // Close the temporary file
+    tmpFile.close();
+
+    // Compile the temporary file
+    if (system("c++ -o rush_output.exe " TMP_PATH))
+    {
+        return -1;
+    }
+
+    // Delete the temporary file
+    if (remove(TMP_PATH))
+    {
+        return -1;
+    }
+
+    // OK
     return 0;
 }
