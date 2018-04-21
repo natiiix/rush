0c567e60652f37f57a8a736d98d2599b283af046
Ivo Meixner <ivo.meixner@se7en.cz>
2018-04-21 21:42:36 +0200

    Add error logging function and function for reading whole files

--- src/support.cpp
+++ src/support.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "support.hpp"
 
 namespace support
@@ -10,7 +11,17 @@ void printLn(const char *const str)
 
 void printLn(const std::string str)
 {
-    std::cout << str << std::endl;
+    printLn(str.c_str());
+}
+
+void printLnErr(const char *const str)
+{
+    std::cerr << "ERROR: " << str << std::endl;
+}
+
+void printLnErr(const std::string str)
+{
+    printLnErr(str.c_str());
 }
 
 std::vector<RegexMatch> getAllMatches(const std::regex reg, const std::string str)
@@ -24,6 +35,11 @@ std::vector<RegexMatch> getAllMatches(const std::regex reg, const std::string st
     }
 
     return matches;
+}
+
+std::vector<RegexMatch> getAllMatches(const std::string reg, const std::string str)
+{
+    return getAllMatches(std::regex(reg), str);
 }
 
 bool isWhitespace(const char c)
@@ -51,5 +67,28 @@ std::string trimWhitespace(const std::string input, int &leadingSpaces)
 
     leadingSpaces = leading;
     return input.substr(leading, input.size() - leading - trailing);
+}
+
+// Source: https://stackoverflow.com/a/2602258/3043260
+std::string readFile(const char *const path)
+{
+    std::ifstream reader(path);
+
+    if (!reader.is_open())
+    {
+        return std::string();
+    }
+
+    reader.seekg(0, std::ios::end);
+
+    size_t size = reader.tellg();
+    std::string buffer(size, ' ');
+
+    reader.seekg(0);
+
+    reader.read(&buffer[0], size);
+    reader.close();
+
+    return buffer;
 }
 }
--- src/support.hpp
+++ src/support.hpp
@@ -10,9 +10,13 @@ namespace support
 {
 void printLn(const char *const str);
 void printLn(const std::string str);
+void printLnErr(const char *const str);
+void printLnErr(const std::string str);
 std::vector<RegexMatch> getAllMatches(const std::regex reg, const std::string str);
+std::vector<RegexMatch> getAllMatches(const std::string reg, const std::string str);
 bool isWhitespace(const char c);
 std::string trimWhitespace(const std::string input, int &leadingSpaces);
+std::string readFile(const char *const path);
 
 template <class T, class U, class F>
 void map(const std::vector<T> src, std::vector<U> &dest, const F f)
