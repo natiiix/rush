615b9de05fbc9e761896cc07022e6841f79a106b
Ivo Meixner <ivo.meixner@se7en.cz>
2018-04-21 21:43:40 +0200

    Improve main procedure of reading source files and compiling

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,89 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <string>
 #include <regex>
-#include <fstream>
 #include <cstring>
 #include <cstdio>
+#include "support.hpp"
+
+using namespace support;
 
 #define TMP_PATH "__rush_temp.cpp"
 
 int main(const int argc, const char *const argv[])
 {
+    // std::string str("Hello <Tag> World! <AnotherTag> Ooops! <ThirdTag>");
+    // // std::smatch mr;
+    // std::regex reg("<(\\w+)>");
+
+    // std::vector<RegexMatch> matches = getAllMatches(reg, str);
+
+    // // if (std::regex_search(str, mr, reg))
+    // // {
+    // //     printLn("Match!");
+    // // }
+    // // else
+    // // {
+    // //     printLn("Not a match!");
+    // // }
+
+    // std::cout << "Found " << matches.size() << " matches!" << std::endl;
+
+    // int n = 0;
+    // for (RegexMatch mr : matches)
+    // {
+    //     std::cout << "-- Match number " << ++n << std::endl;
+
+    //     std::cout
+    //         << "Empty: " << mr.empty()
+    //         << " Size: " << mr.size()
+    //         << " Prefix: " << mr.prefix()
+    //         << " Suffix: " << mr.suffix()
+    //         << " Length: " << mr.length()
+    //         << " Position: " << mr.position()
+    //         << " Str: " << mr.str() << std::endl;
+
+    //     std::for_each(mr.begin(), mr.end(), [](const std::string x) -> void { printLn(x); });
+    // }
+
+    // return 0;
+
     // Not enough arguments
     if (argc < 3)
     {
-        std::cout << "Expected syntax: rush <Output File> <Source Files>" << std::endl;
+        printLnErr("Expected syntax: rush <Output File> <Source Files>");
         return -1;
+    }
+
+    // Create a vector to hold the code from the source files
+    std::vector<std::string> sourceFiles;
+
+    // Read the source files
+    for (int i = 2; i < argc; i++)
+    {
+        const char *const path = argv[i];
+
+        std::cout << "Reading source file: " << path << std::endl;
+        std::string code = readFile(path);
+
+        if (code.empty())
+        {
+            printLnErr("Unable to read source file / source file is empty");
+            return -1;
+        }
+        else
+        {
+            sourceFiles.push_back(code);
+        }
+    }
+
+    // TODO: Process the source file
+    std::stringstream tempCode;
+
+    for (std::string code : sourceFiles)
+    {
+        tempCode << code << std::endl;
     }
 
     // Open the temporary file
@@ -22,31 +92,12 @@ int main(const int argc, const char *const argv[])
     // Unable to open the temporary file
     if (!tmpFile.is_open())
     {
+        printLnErr("Unable to create a temporary file");
         return -1;
     }
 
-    // Iterate through the source files
-    for (int i = 2; i < argc; i++)
-    {
-        const char *fileName = argv[i];
-
-        std::cout << "Reading source file: " << fileName << std::endl;
-        std::ifstream sourceFile(fileName);
-
-        if (!sourceFile.is_open())
-        {
-            std::cerr << "Unable to read source file \"" << fileName << "\"!" << std::endl;
-            continue;
-        }
-
-        std::string line;
-        while (std::getline(sourceFile, line))
-        {
-            tmpFile << line << std::endl;
-        }
-
-        sourceFile.close();
-    }
+    // Write the temporary code to the temporary file
+    tmpFile << tempCode.rdbuf();
 
     // Close the temporary file
     tmpFile.close();
@@ -57,17 +108,22 @@ int main(const int argc, const char *const argv[])
     compileCmd += " ";
     compileCmd += TMP_PATH;
 
+    printLn("Compiling temporary code...");
+
     if (system(compileCmd.c_str()))
     {
+        printLnErr("Unable to compile the temporary code");
         return -1;
     }
 
     // Delete the temporary file
     if (remove(TMP_PATH))
     {
+        printLnErr("Unable to delete the temporary file");
         return -1;
     }
 
-    // OK
+    // Everything worked correctly
+    printLn("OK");
     return 0;
 }
