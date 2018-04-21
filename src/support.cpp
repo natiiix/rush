7687998bd1cc8f76ad341f07a6ebaf646bb5c300
Ivo Meixner <ivo.meixner@se7en.cz>
2018-04-22 01:40:52 +0200

    Add bunch of string-related functions

--- src/support.cpp
+++ src/support.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include "support.hpp"
 
 namespace support
@@ -47,7 +48,7 @@ bool isWhitespace(const char c)
     return c == ' ' || c == '\t';
 }
 
-std::string trimWhitespace(const std::string input, int &leadingSpaces)
+std::string trimWhitespace(const std::string input, int &indentation)
 {
     int leading = 0;
     int trailing = 0;
@@ -57,15 +58,19 @@ std::string trimWhitespace(const std::string input, int &leadingSpaces)
         leading++;
     }
 
-    if (leading < input.size())
+    indentation = leading;
+
+    // The whole string contains nothing but whitespace
+    if (leading == input.size())
     {
-        for (int i = input.size() - 1; i >= 0 && isWhitespace(input[i]); i++)
-        {
-            trailing++;
-        }
+        return std::string();
     }
 
-    leadingSpaces = leading;
+    for (int i = input.size() - 1; i >= 0 && isWhitespace(input[i]); i--)
+    {
+        trailing++;
+    }
+
     return input.substr(leading, input.size() - leading - trailing);
 }
 
@@ -90,5 +95,67 @@ std::string readFile(const char *const path)
     reader.close();
 
     return buffer;
+}
+
+// Source: https://stackoverflow.com/a/24315631/3043260
+void replaceAllInPlace(std::string &str, const std::string &from, const std::string &to)
+{
+    // Cannot replace an empty string
+    if (from.empty())
+    {
+        return;
+    }
+
+    size_t start_pos = 0;
+    while ((start_pos = str.find(from, start_pos)) != std::string::npos)
+    {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length();
+    }
+}
+
+std::string replaceAll(const std::string &str, const std::string &from, const std::string &to)
+{
+    std::string newStr = str;
+    replaceAllInPlace(newStr, from, to);
+    return newStr;
+}
+
+// Source: https://stackoverflow.com/a/14267455/3043260
+std::vector<std::string> split(const std::string str, const std::string delimiter)
+{
+    std::vector<std::string> tokens;
+
+    size_t start = 0;
+    size_t end = str.find(delimiter);
+    while (end != std::string::npos)
+    {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + delimiter.length();
+        end = str.find(delimiter, start);
+    }
+
+    tokens.push_back(str.substr(start, end));
+
+    return tokens;
+}
+
+// Source: https://stackoverflow.com/a/1430774/3043260
+std::string join(const std::string delimiter, const std::vector<std::string> values)
+{
+    std::stringstream ss;
+
+    for (size_t i = 0; i < values.size(); ++i)
+    {
+        if (i)
+        {
+            ss << delimiter;
+        }
+
+        ss << values[i];
+    }
+
+    return ss.str();
 }
 }
--- src/support.hpp
+++ src/support.hpp
@@ -15,8 +15,12 @@ void printLnErr(const std::string str);
 std::vector<RegexMatch> getAllMatches(const std::regex reg, const std::string str);
 std::vector<RegexMatch> getAllMatches(const std::string reg, const std::string str);
 bool isWhitespace(const char c);
-std::string trimWhitespace(const std::string input, int &leadingSpaces);
+std::string trimWhitespace(const std::string input, int &indentation);
 std::string readFile(const char *const path);
+void replaceAllInPlace(std::string &str, const std::string &from, const std::string &to);
+std::string replaceAll(const std::string &str, const std::string &from, const std::string &to);
+std::vector<std::string> split(const std::string str, const std::string delimiter);
+std::string join(const std::string delimiter, const std::vector<std::string> values);
 
 template <class T, class U, class F>
 void map(const std::vector<T> src, std::vector<U> &dest, const F f)
