1905cca1d889c6a95bc4cef8464380682b3bafe1
Ivo Meixner <ivo.meixner@se7en.cz>
2018-04-23 01:42:41 +0200

    Make compilation process slightly more verbose (one message per step)

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,11 @@ using namespace support;
 
 #define TMP_PATH "__rush_temp.cpp"
 
+void log(const char *const str)
+{
+    std::cout << str << " ... ";
+}
+
 int main(const int argc, const char *const argv[])
 {
     // Not enough arguments
@@ -25,7 +30,7 @@ int main(const int argc, const char *const argv[])
     {
         const char *const path = argv[i];
 
-        std::cout << "Reading source file: " << path << std::endl;
+        std::cout << "Reading source file: " << path << " ... ";
         std::string code = readFile(path);
 
         if (code.empty())
@@ -35,21 +40,29 @@ int main(const int argc, const char *const argv[])
         }
         else
         {
+            printLn("OK");
             sourceFiles.push_back(code);
         }
     }
 
     // Compile the source files into temporary code
+    log("Compiling the source files into a temporary code");
+
     Compiler comp(sourceFiles);
     std::string tempCode = comp.compile();
 
     // Compile error occurred
     if (tempCode.empty())
     {
+        printLnErr("Unable to compile the source files");
         return -1;
     }
 
+    printLn("OK");
+
     // Open the temporary file
+    log("Creating a temporary file");
+
     std::ofstream tmpFile(TMP_PATH);
 
     // Check if the temporary file has been correctly opened
@@ -65,13 +78,15 @@ int main(const int argc, const char *const argv[])
     // Close the temporary file
     tmpFile.close();
 
+    printLn("OK");
+
     // Compile the temporary file
+    log("Compiling the temporary code into a binary executable");
+
     std::string compileCmd("c++ -o ");
     compileCmd += argv[1];
     compileCmd += " ";
     compileCmd += TMP_PATH;
-
-    printLn("Compiling temporary code...");
 
     if (system(compileCmd.c_str()))
     {
@@ -79,14 +94,19 @@ int main(const int argc, const char *const argv[])
         return -1;
     }
 
+    printLn("OK");
+
     // Delete the temporary file
+    // log("Deleting the temporary file");
+
     // if (remove(TMP_PATH))
     // {
     //     printLnErr("Unable to delete the temporary file");
     //     return -1;
     // }
 
+    // printLn("OK");
+
     // Everything worked correctly
-    printLn("OK");
     return 0;
 }
