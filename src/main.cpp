458634a8c9c6aacde5ca11946fb6f510dbf1f7ce
Ivo Meixner <ivo.meixner@se7en.cz>
2018-04-24 03:35:51 +0200

    Change main() to use new file reading logic

--- src/main.cpp
+++ src/main.cpp
@@ -6,49 +6,21 @@
 
 using namespace support;
 
-#define TMP_PATH "__rush_temp.cpp"
-
-void log(const char *const str)
-{
-    std::cout << str << " ... ";
-}
-
 int main(const int argc, const char *const argv[])
 {
     // Not enough arguments
-    if (argc < 3)
+    if (argc != 3)
     {
-        printLnErr("Expected syntax: rush <Output File> <Source Files>");
+        printLnErr("Expected syntax: rush <Output File> <Main Source File>");
         return -1;
     }
 
-    // Create a vector to hold the code from the source files
-    std::vector<std::string> sourceFiles;
-
-    // Read the source files
-    for (int i = 2; i < argc; i++)
-    {
-        const char *const path = argv[i];
-
-        std::cout << "Reading source file: " << path << " ... ";
-        std::string code = readFile(path);
-
-        if (code.empty())
-        {
-            printLnErr("Unable to read source file / source file is empty");
-            return -1;
-        }
-        else
-        {
-            printLn("OK");
-            sourceFiles.push_back(code);
-        }
-    }
+    // Create a compiler
+    Compiler comp(normalizePath(argv[2]));
 
     // Compile the source files into temporary code
     log("Compiling the source files into a temporary code");
 
-    Compiler comp(sourceFiles);
     std::string tempCode = comp.compile();
 
     // Compile error occurred
@@ -58,12 +30,14 @@ int main(const int argc, const char *const argv[])
         return -1;
     }
 
-    printLn("OK");
+    ok();
 
     // Open the temporary file
     log("Creating a temporary file");
 
-    std::ofstream tmpFile(TMP_PATH);
+    const std::string tmpPath = "__rush_temp.cpp";
+
+    std::ofstream tmpFile(tmpPath);
 
     // Check if the temporary file has been correctly opened
     if (!tmpFile.is_open())
@@ -78,15 +52,12 @@ int main(const int argc, const char *const argv[])
     // Close the temporary file
     tmpFile.close();
 
-    printLn("OK");
+    ok();
 
     // Compile the temporary file
     log("Compiling the temporary code into a binary executable");
 
-    std::string compileCmd("c++ -o ");
-    compileCmd += argv[1];
-    compileCmd += " ";
-    compileCmd += TMP_PATH;
+    std::string compileCmd = "c++ -o " + std::string(argv[1]) + " " + tmpPath;
 
     if (system(compileCmd.c_str()))
     {
@@ -94,18 +65,18 @@ int main(const int argc, const char *const argv[])
         return -1;
     }
 
-    printLn("OK");
+    ok();
 
     // Delete the temporary file
     // log("Deleting the temporary file");
 
-    // if (remove(TMP_PATH))
+    // if (remove(tmpPath.c_str()))
     // {
     //     printLnErr("Unable to delete the temporary file");
     //     return -1;
     // }
 
-    // printLn("OK");
+    // ok();
 
     // Everything worked correctly
     return 0;
