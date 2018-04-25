c117c38de651f4150950a50bb884888f25b3fe3a
Ivo Meixner <ivo.meixner@se7en.cz>
2018-04-26 00:45:13 +0200

    Remove unnecessary #include from CodeLine

--- src/CodeLine.cpp
+++ src/CodeLine.cpp
@@ -1,7 +1,4 @@
 #include "CodeLine.hpp"
-#include "support.hpp"
-
-using namespace support;
 
 CodeLine::CodeLine(const std::string origin, const int number, const std::string code, const int indentation) : origin(origin), number(number), code(code), indentation(indentation)
 {
