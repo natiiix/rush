b0511afb22f5b2e8a411a8f866c29bc143260cb1
Ivo Meixner <ivo.meixner@se7en.cz>
2018-04-26 01:36:57 +0200

    Change syntax to allow optional compiler arguments

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,26 @@ using namespace support;
 int main(const int argc, const char *const argv[])
 {
     // Not enough arguments
-    if (argc != 3)
+    if (argc < 3)
     {
-        printLnErr("Expected syntax: rush <Output File> <Main Source File>");
+        printLnErr("Expected syntax: rush <Output File> <Main Source File> [--keep] [C++ Compiler Arguments]");
         return -1;
     }
 
-    // Create a compiler
-    Compiler comp(normalizePath(argv[2]));
+    log("Loading source files");
 
-    // Compile the source files into temporary code
+    // Get the main source file's path
+    const std::string srcPath = normalizePath(argv[2]);
+
+    // Create a compiler
+    const Compiler comp(srcPath);
+
+    ok();
+
     log("Compiling the source files into a temporary code");
 
-    std::string tempCode = comp.compile();
+    // Compile the source files into temporary code
+    const std::string tempCode = comp.compile();
 
     // Compile error occurred
     if (tempCode.empty())
@@ -35,7 +42,7 @@ int main(const int argc, const char *const argv[])
     // Open the temporary file
     log("Creating a temporary file");
 
-    const std::string tmpPath = "__rush_temp.cpp";
+    const std::string tmpPath = srcPath + ".tmp.cpp";
 
     std::ofstream tmpFile(tmpPath);
 
@@ -54,10 +61,28 @@ int main(const int argc, const char *const argv[])
 
     ok();
 
+    // Process optional arguments
+    bool deleteTmpFile = true;
+    std::string compilerArgs;
+
+    for (int i = 3; i < argc; i++)
+    {
+        std::string arg = argv[i];
+
+        if (i == 3 && arg == "--keep")
+        {
+            deleteTmpFile = false;
+        }
+        else
+        {
+            compilerArgs += " " + wrapInQuotesIfContainsSpace(arg);
+        }
+    }
+
     // Compile the temporary file
     log("Compiling the temporary code into a binary executable");
 
-    std::string compileCmd = "c++ -o " + std::string(argv[1]) + " " + tmpPath;
+    std::string compileCmd = "c++ -o " + wrapInQuotesIfContainsSpace(argv[1]) + " " + wrapInQuotesIfContainsSpace(tmpPath) + compilerArgs;
 
     if (system(compileCmd.c_str()))
     {
@@ -67,16 +92,24 @@ int main(const int argc, const char *const argv[])
 
     ok();
 
-    // Delete the temporary file
-    // log("Deleting the temporary file");
+    // Delete the temporary file unless the user has specified they want to keep it
+    if (deleteTmpFile)
+    {
+        log("Deleting the temporary file");
 
-    // if (remove(tmpPath.c_str()))
-    // {
-    //     printLnErr("Unable to delete the temporary file");
-    //     return -1;
-    // }
+        if (remove(tmpPath.c_str()))
+        {
+            printLnErr("Unable to delete the temporary file");
+            return -1;
+        }
 
-    // ok();
+        ok();
+    }
+    else
+    {
+        log("Keeping the temporary file");
+        ok();
+    }
 
     // Everything worked correctly
     return 0;
